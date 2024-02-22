8a1fb1c182573eda3b946bee485ed96c1dd3a218
KEERTHANA E <100853392+keerysbib@users.noreply.github.com>
2024-02-22 11:55:10 +0530

    new changes

--- src/main/java/com/training/testdriveapp/customer/CustomerServices.java
+++ src/main/java/com/training/testdriveapp/customer/CustomerServices.java
@@ -1,7 +1,22 @@
 package com.training.testdriveapp.customer;
 
 import com.training.testdriveapp.customer.Customer;
+import com.training.testdriveapp.rating.Rating;
+
+import java.util.List;
 
 public interface CustomerServices {
-    Customer addNewCustomer(Customer newCustomer);
+    Customer addNewCustomer(Customer newCustomer) throws CustomerException;
+
+    List<Customer> getAllCustomers();
+
+    Customer updateCustomer(Customer customer) throws CustomerException;
+
+    void deleteCustomer(Integer id) throws CustomerException;
+
+    Customer login(LoginDto loginDto) throws CustomerException;
+
+    Customer getCustomerById(Integer customerId);
+
+    List<Rating> getCustomerRating(Integer id);
 }
