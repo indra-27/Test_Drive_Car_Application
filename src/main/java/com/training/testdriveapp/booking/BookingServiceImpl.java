bb9ab6f9fbcfd07b279e26d5631605a64b6aead6
Indra <indhukavibala2003@gmail.com>
2024-02-22 12:39:15 +0530

    Update BookingServiceImpl.java

--- src/main/java/com/training/testdriveapp/booking/BookingServiceImpl.java
+++ src/main/java/com/training/testdriveapp/booking/BookingServiceImpl.java
@@ -1,15 +1,25 @@
 package com.training.testdriveapp.booking;
 
+import com.training.testdriveapp.admin.Car;
+import com.training.testdriveapp.admin.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookingServiceImpl implements BookingService{
 
     @Autowired
     private BookingRepository bookingRepository;
+
+    @Autowired
+    private CarRepository carRepository;
     @Override
     public Booking createNewBooking(BookingDto newBooking) {
-        return this.bookingRepository.save(new Booking());
+        List<Car> carDetails = carRepository.findBymodelName(newBooking.getCarModelName());
+        Integer carId = carDetails.getFirst().getCarId();
+        this.bookingRepository.save(new Booking());
+        return null;
     }
 }
