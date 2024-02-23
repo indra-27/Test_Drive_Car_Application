42936e7c31e10c0d4c6509a73b0d598d79029195
deepaaumapathi <deepaaumapathi04@gmail.com>
2024-02-23 14:04:11 +0530

    changes 1

--- src/main/java/com/training/testdriveapp/staff/StaffController.java
+++ src/main/java/com/training/testdriveapp/staff/StaffController.java
@@ -1,4 +1,36 @@
 package com.training.testdriveapp.staff;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+@RestController
 public class StaffController {
+    @Autowired
+    private StaffService staffService;
+
+    @PostMapping("staff")
+    public Staff addNewStaff(Staff newStaff) throws StaffException
+    {
+        return this.staffService.addNewStaff(newStaff);
+    }
+
+    @PutMapping("Update Staff")
+    public Staff updateAccount(@RequestBody Staff staff) throws StaffException {
+        return this.staffService.updateStaffDetails(staff);
+    }
+
+    @GetMapping("Get Staff by Id/{staffid}")
+    public Staff getByStaffId(@PathVariable("staffid") Integer staffid) throws StaffException {
+        return this.staffService.getByStaffId(staffid);
+    }
+    @DeleteMapping("Delete Staff/{staffid}")
+    public Optional<Staff> deleteAccountById(@PathVariable Integer staffid) throws StaffException {
+        return this.staffService.deleteStaff(staffid);
+    }
+    @GetMapping("getAllStaffs")
+    public List<Staff> getAllAccounts() throws StaffException {
+        return this.staffService.getAllStaffs();
+    }
 }
--- src/main/java/com/training/testdriveapp/staff/StaffService.java
+++ src/main/java/com/training/testdriveapp/staff/StaffService.java
@@ -1,4 +1,16 @@
 package com.training.testdriveapp.staff;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface StaffService {
+    Staff addNewStaff(Staff newStaff) throws StaffException;
+
+    Staff updateStaffDetails(Staff staff) throws StaffException;
+
+    Staff getByStaffId(Integer staffId) throws StaffException;
+
+    Optional<Staff> deleteStaff(Integer staffId) throws StaffException;
+
+    List<Staff> getAllStaffs() throws StaffException;
 }
